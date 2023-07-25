50e2b068d7be6004d32e52c545d92e00172d97cf
Sarasima <rosetruckchlori@protonmail.com>
2023-07-25 22:14:50 +0300

    init

---
+++ src/main/java/com/tickething/tickething/TickethingApplication.java
@@ -0,0 +1,13 @@
+package com.tickething.tickething;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickethingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickethingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tickething/tickething/TickethingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tickething.tickething;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickethingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
